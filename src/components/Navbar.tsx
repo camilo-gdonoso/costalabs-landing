57bd1e6547c74f9801a10210d0a3b1401650fc57
Camilo Gonzalez <camilo.gdonoso@gmail.com>
2026-02-26 12:06:58 -0400

    banner

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -47,7 +47,7 @@ export default function Navbar() {
                                 alignItems: 'baseline'
                             }}
                         >
-                            CostaLabs
+                            ostaLabs
                         </Typography>
                     </Box>
 
